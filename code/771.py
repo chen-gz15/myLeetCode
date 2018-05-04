f1dea365ecec48e316a749351fdf4c4de96828f1
chen-gz15 <37355098+chen-gz15@users.noreply.github.com>
2018-05-05 00:47:01 +0800

    Update 771.py

--- code/771.py
+++ code/771.py
@@ -1,1 +1,29 @@
- 
+#my code
+class Solution(object):
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        count=0
+        for i in range(len(S)):
+            for j in range(len(J)):
+                if S[i]==J[j]:
+                    count=count+1
+        return count
+       
+#better code
+class Solution(object):
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        count = 0
+        J = set(J)
+        for i in S:
+            if i in J:
+                count += 1
+        return count
